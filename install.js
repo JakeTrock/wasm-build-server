44772bee3116611649f450e2d669f88628b41ac2
RepComm <dev@jonathancrowder.com>
2019-03-04 02:47:56 -0700

    Added execute perms to emsdk and corrected execution commands

--- install.js
+++ install.js
@@ -6,13 +6,14 @@
 const fetch = require("node-fetch"); //So we can fetch tools from online
 const AdmZip = require("adm-zip"); //Decompressing zip files
 const child_process = require("child_process"); //Running commands
+const fs = require("fs");
 const path = require("path"); //Non-insane file path management
 
 //emsdk's github download link
 let emsdkUrl = "https://github.com/emscripten-core/emsdk/archive/master.zip";
 
 //desired emsdk install directory
-let unzippedDest = "./emsdk";
+let unzippedDest = "emsdk";
 
 console.log("Fetching emsdk");
 fetch(emsdkUrl).then((resp)=>{
@@ -29,16 +30,16 @@ fetch(emsdkUrl).then((resp)=>{
         console.log("Installing emsdk");
 
         let executablePath = path.join(__dirname, unzippedDest);
-
+        fs.chmodSync(path.join(executablePath, "emsdk"), "777");
         //Call install functionality of emsdk
-        child_process.exec("emsdk install latest", {
+        child_process.exec("./emsdk install latest", {
             cwd:executablePath
         }, (ex, out, err)=>{
             if (!ex && !err) {
                 console.log("Activating emsdk");
 
                 //Call activate functionality of emsdk
-                child_process.exec("emsdk activate latest", {
+                child_process.exec("./emsdk activate latest", {
                     cwd:executablePath
                 }, (ex1, out1, err1)=>{
                     if (!ex1 && !err1) {
