25d6de1fcc14e65c756a5ab859fa1620c8c04dfa
RepComm <info@jonathancrowder.com>
2019-03-11 19:21:35 -0600

    Projects fetch when user logged in and displayed, Formatted CSS doc, Added some JS doc to index.js of client, Simplified login logic

--- client/index.js
+++ client/index.js
@@ -50,6 +50,10 @@ const getCookie = (key)=> {
     return "";
 }
 
+/** Combine url with arguments
+ * @param {String} url to use
+ * @param {Map} args to parse
+ */
 const urlWithArgs = (url, args)=> {
     let keys = Object.keys(args);
     let key, val;
@@ -68,6 +72,11 @@ const urlWithArgs = (url, args)=> {
     return url + appendStr;
 }
 
+/** Login using email and password
+ * @param {String} email of user
+ * @param {String} pass of user
+ * @param {Function} cb callback
+ */
 const login = (email, pass, cb)=> {
     fetch(urlWithArgs(apiUrl, {
         type:"login",
@@ -78,23 +87,43 @@ const login = (email, pass, cb)=> {
     });
 }
 
+/** Logout, deleting the user cookie
+ */
 const logout = ()=>{
     removeCookie("wasm-frontend-user-cookie");
 }
 
+/** Get details of a user using their cookie
+ * @param {String} cookie of user
+ * @param {Function} cb callback
+ */
 const details = (cookie, cb)=>{
     fetch(urlWithArgs(apiUrl, {
         type:"details",
         "wasm-frontend-user-cookie":cookie
     })).then((response)=>{
         response.json().then(cb);
-    })
+    });
+}
+
+/** Fetch all projects data of a profile
+ * @param {Integer} owner id of projects to fetch
+ * @param {Function} cb callback
+ */
+const projectList = (owner, cb) => {
+    fetch(urlWithArgs(apiUrl, {
+        type:"projects-list",
+        owner:owner
+    })).then((response)=>{
+        response.json().then(cb);
+    });
 }
 
 let cookie = getCookie("wasm-frontend-user-cookie");
 
 let get = (id)=>document.getElementById(id);
 let on = (e, type, cb, opts) => e.addEventListener(type, cb, opts);
+
 let isLoggedIn = ()=>{
     let c = getCookie("wasm-frontend-user-cookie");
     return c !== "" && c !== "undefined";
@@ -104,12 +133,46 @@ let sUserName = get("username");
 let bLoginout = get("loginout");
 let iEmail = get("email");
 let iPass = get("pass");
+let dProjects = get("projects");
 
 if (iEmail.value == "") {
     iEmail.value = "demo@email.com";
 }
 if (iPass.value == "") {
     iPass.value = "apassword";
+}
+
+let onLogin = ()=>{
+    details(getCookie("wasm-frontend-user-cookie"), (data)=>{
+        console.log(data);
+        sUserName.textContent = "Welcome, " + data.details.display;
+        iEmail.style.display = "none";
+        iPass.style.display = "none";
+        bLoginout.textContent = "Logout";
+
+        projectList(data.details.id, (data0)=>{
+            let proj;
+            for (let i=0; i<data0.projects.length; i++) {
+                proj = data0.projects[i];
+                
+                let dProj = document.createElement("div");
+                dProj.className = "project";
+
+                let sTitle = document.createElement("span");
+                sTitle.className = "project-title";
+                sTitle.textContent = proj.name;
+                dProj.appendChild(sTitle);
+
+                let sDesc = document.createElement("span");
+                sDesc.className = "project-desc";
+                sDesc.textContent = proj.description;
+                sDesc.id = "p-" + proj.id;
+                dProj.appendChild(sDesc);
+                
+                dProjects.appendChild(dProj);
+            }
+        });
+    });
 }
 
 on(bLoginout, "click", ()=>{
@@ -125,24 +188,12 @@ on(bLoginout, "click", ()=>{
                 setCookie("wasm-frontend-user-cookie", resp["wasm-frontend-user-cookie"]);
                 console.log(resp);
 
-                details(getCookie("wasm-frontend-user-cookie"), (data)=>{
-                    console.log(data);
-                    sUserName.textContent = "Welcome, " + data.details.display;
-                    iEmail.style.display = "none";
-                    iPass.style.display = "none";
-                    bLoginout.textContent = "Logout";
-                });
+                onLogin();
             }
         });
     }
 });
 
 if (isLoggedIn()) {
-    details(getCookie("wasm-frontend-user-cookie"), (data)=>{
-        console.log(data);
-        sUserName.textContent = "Welcome, " + data.details.display;
-        iEmail.style.display = "none";
-        iPass.style.display = "none";
-        bLoginout.textContent = "Logout";
-    });
+    onLogin();
 }
